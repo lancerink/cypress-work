80657931f7226c83a67614bbe2d46156a3e89808
lancerink <lenorsink@gmail.com>
2021-08-24 20:11:23 +1200

    Add comments
    add questions

--- cypress/integration/Course-management test/test3.js
+++ cypress/integration/Course-management test/test3.js
@@ -8,12 +8,12 @@ describe ('Login form',() => {
         cy.get('#login_role > :nth-child(3)').click()
         cy.get('#login_email').type('manager@admin.com')
         cy.get('#login_password').type('111111')
-        cy.get('[type="submit"').click()
+        cy.get('[type="submit"').click() // how to add command?
         cy.wait(9000)
         cy.url().should('include','dashboard/manager')
         cy.visit('/dashboard/manager/courses/add-course')
         cy.get('#name').type('test course')
-        //cy.get('#teacherId').type('a')
+        //cy.get('#teacherId').type('a') //unable to select teacher, it's still Off after logging in
         //cy.get('ant-select-item-option').first().click()
         cy.get('#type').click()
         cy.get('[title~=PHP]').click()
@@ -25,7 +25,6 @@ describe ('Login form',() => {
         cy.get('.ant-input-group > .ant-input-number > .ant-input-number-input-wrap > .ant-input-number-input').type('5') //any other method on selecting?
         cy.get('[placeholder="Course description"]').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500ss popularised in the 1960s')
         cy.get('[type="submit"]').click
-
 
     })
 
